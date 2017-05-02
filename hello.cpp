f0a1c7ebd85b4020b445bbb4e6f5bb5ee95c7a87
khai96_ <hvksmr1996@gmail.com>
2017-05-03 02:25:03 +0700

    Simpler is better

--- hello.cpp
+++ hello.cpp
@@ -3,16 +3,17 @@
 #define __HELLO_CPP
 
 #include "hello.hpp"
-
-App::App (int argc, char ** argv) :
-  QApplication(argc, argv), label("Hello, World!!")
-{
-  window.setCentralWidget(&label);
-  window.show();
-}
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QLabel>
 
 Hello::Hello (int argc, char ** argv) {
-  status = App(argc, argv).exec();
+  QApplication app(argc, argv);
+  QMainWindow window;
+  QLabel label("Hello, World!!");
+  window.setCentralWidget(&label);
+  window.show();
+  app.exec();
 }
 
 #endif
--- hello.hpp
+++ hello.hpp
@@ -2,18 +2,6 @@
 #ifndef __HELLO_HPP
 #define __HELLO_HPP
 
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QMainWindow>
-#include <QtWidgets/QLabel>
-
-class App : public QApplication {
-private:
-  QMainWindow window;
-  QLabel label;
-public:
-  App (int argc, char ** argv);
-};
-
 class Hello {
 public:
   Hello (int, char **);

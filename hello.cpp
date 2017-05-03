1eddc5f36e8932460cd662fc6a74fc2bf0abefe7
khai96_ <hvksmr1996@gmail.com>
2017-05-03 07:39:34 +0700

    A bit more complicated Because OOP! The more overcomplicated, the more OOP! Fuck yeah!

--- hello.cpp
+++ hello.cpp
@@ -3,14 +3,11 @@
 #define __HELLO_CPP
 
 #include "hello.hpp"
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QMainWindow>
-#include <QtWidgets/QLabel>
 
 Hello::Hello (int argc, char ** argv) {
   QApplication app(argc, argv);
-  QMainWindow window;
-  QLabel label("Hello, World!!");
+  MainWin window;
+  Label label("Hello, World!!");
   window.setCentralWidget(&label);
   window.show();
   app.exec();
--- hello.hpp
+++ hello.hpp
@@ -2,7 +2,24 @@
 #ifndef __HELLO_HPP
 #define __HELLO_HPP
 
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QLabel>
+
+// template <class Base>
+// using SubClass = Base;
+
+template <class Base>
+struct SubClass : Base {
+  template <typename ...Args> SubClass (Args ...args) : Base(args ...) {}
+};
+
 class Hello {
+private:
+  typedef SubClass<QApplication> App;
+  typedef SubClass<QMainWindow> MainWin;
+  typedef SubClass<QLabel> Label;
+
 public:
   Hello (int, char **);
   int status;

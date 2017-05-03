302e9a513b670805e0c967402322682bb51e7351
khai96_ <hvksmr1996@gmail.com>
2017-05-03 12:08:40 +0700

    Move to utils

--- hello.hpp
+++ hello.hpp
@@ -6,25 +6,7 @@
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QLabel>
 #include <experimental/optional>
-
-template <class Base>
-struct DerivedClass : public Base {
-  template <typename ...Args> DerivedClass (Args & ...args) : Base(args ...) {}
-};
-
-template <typename Base>
-using Nullable = std::experimental::optional<Base>;
-
-template <typename Base, Base byDefault>
-class Optional : public Nullable<Base> {
-private:
-  typedef Nullable<Base> NBase;
-public:
-  template <typename ...Args> Optional (Args ...args) : NBase(args ...) {}
-  operator Base () {
-    return NBase::value_or(byDefault);
-  }
-};
+#include "lib/utils.hpp"
 
 class Hello {
 private:
---
+++ lib/utils.hpp
@@ -0,0 +1,24 @@
+
+#ifndef __LIB_UTILS_HPP
+#define __LIB_UTILS_HPP
+
+template <class Base>
+struct DerivedClass : public Base {
+  template <typename ...Args> DerivedClass (Args & ...args) : Base(args ...) {}
+};
+
+template <typename Base>
+using Nullable = std::experimental::optional<Base>;
+
+template <typename Base, Base byDefault>
+class Optional : public Nullable<Base> {
+private:
+  typedef Nullable<Base> NBase;
+public:
+  template <typename ...Args> Optional (Args ...args) : NBase(args ...) {}
+  operator Base () {
+    return NBase::value_or(byDefault);
+  }
+};
+
+#endif

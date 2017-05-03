d467253df21e46296cf8ef07e0480daa26649a47
khai96_ <hvksmr1996@gmail.com>
2017-05-03 11:58:43 +0700

    Create Optional and Nullable

--- hello.hpp
+++ hello.hpp
@@ -5,10 +5,25 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QLabel>
+#include <experimental/optional>
 
 template <class Base>
 struct DerivedClass : public Base {
   template <typename ...Args> DerivedClass (Args & ...args) : Base(args ...) {}
+};
+
+template <typename Base>
+using Nullable = std::experimental::optional<Base>;
+
+template <typename Base, Base byDefault>
+class Optional {
+private:
+  Nullable<Base> base;
+public:
+  template <typename ...Args> Optional (Args ...args) : base(args ...) {}
+  operator Base () {
+    return base.value_or(byDefault);
+  }
 };
 
 class Hello {
@@ -19,7 +34,7 @@ private:
 
 public:
   Hello (int, char **);
-  int status;
+  Optional<int, -1> status;
 };
 
 #endif

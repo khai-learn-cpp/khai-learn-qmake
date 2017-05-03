6e6593f92c637eb59bede7e9b74896d8a2660fec
khai96_ <hvksmr1996@gmail.com>
2017-05-03 12:30:31 +0700

    Indent

--- lib/utils.hpp
+++ lib/utils.hpp
@@ -6,24 +6,24 @@
 
 namespace lib::utils {
 
-template <class Base>
-struct DerivedClass : public Base {
-  template <typename ...Args> DerivedClass (Args & ...args) : Base(args ...) {}
-};
+  template <class Base>
+  struct DerivedClass : public Base {
+    template <typename ...Args> DerivedClass (Args & ...args) : Base(args ...) {}
+  };
 
-template <typename Base>
-using Nullable = std::experimental::optional<Base>;
+  template <typename Base>
+  using Nullable = std::experimental::optional<Base>;
 
-template <typename Base, Base byDefault>
-class Optional : public Nullable<Base> {
-private:
-  typedef Nullable<Base> NBase;
-public:
-  template <typename ...Args> Optional (Args ...args) : NBase(args ...) {}
-  operator Base () {
-    return NBase::value_or(byDefault);
-  }
-};
+  template <typename Base, Base byDefault>
+  class Optional : public Nullable<Base> {
+  private:
+    typedef Nullable<Base> NBase;
+  public:
+    template <typename ...Args> Optional (Args ...args) : NBase(args ...) {}
+    operator Base () {
+      return NBase::value_or(byDefault);
+    }
+  };
 
 }
 
